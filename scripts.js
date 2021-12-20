bce5fe2872477554772593214a457aa2edbd63da
TihomirTruvor <46653043+TihomirTruvor@users.noreply.github.com>
2021-12-20 11:19:47 +0300

    31
    заполнение сайта исправление верски и т.д

--- scripts.js
+++ scripts.js
@@ -1,22 +1,3 @@
-	// функция показать поиск
-	document.querySelector('.mobile-search-button').onclick = function() {
-		
-		let searchContent = document.querySelector('.search');
-		searchContent.style.display = 'flex'; // Открываем поле ввода
-
-		document.querySelector('.mobile-search-button').style.display = 'none'; //Скрываем кнопку поиска
-
-		document.querySelector('.mobile-close-search').style.display = 'block'; // Открываем кнопку закрытия поиска
-
-		document.querySelector('.mobile-close-search').onclick = function(){ // Закрываем поле ввода
-			if (searchContent.style.display = 'block') {
-				searchContent.style.display = 'none';
-				document.querySelector('.mobile-close-search').style.display = 'none';
-				document.querySelector('.mobile-search-button').style.display = 'block'; //Открываем кнопку поиска
-			}
-		}
-	};
-
 // Меню
 	var el = document.getElementsByClassName('nav-item');
 
@@ -90,6 +71,77 @@
 			heading.html([first_part, ' <span class="last-word">', last_word, '</span>'].join(''));
 		});
 	});
+
+function onEntry(entry) {
+	entry.forEach(change => {
+		if (change.isIntersecting) {
+			change.target.classList.add('element-show');
+		}
+	});
+}
+let options = { threshold: [0.2] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.el-animation');
+for (let elm of elements) {
+	observer.observe(elm);
+}
+
+const details = Array.from(document.querySelectorAll("details"));
+
+// Add the onclick listeners.
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
+	// функция показать поиск
+	/*document.querySelector('.mobile-search-button').onclick = function() {
+		
+		let searchContent = document.querySelector('.search');
+		searchContent.style.display = 'flex'; // Открываем поле ввода
+
+		document.querySelector('.mobile-search-button').style.display = 'none'; //Скрываем кнопку поиска
+
+		document.querySelector('.mobile-close-search').style.display = 'block'; // Открываем кнопку закрытия поиска
+
+		document.querySelector('.mobile-close-search').onclick = function(){ // Закрываем поле ввода
+			if (searchContent.style.display = 'block') {
+				searchContent.style.display = 'none';
+				document.querySelector('.mobile-close-search').style.display = 'none';
+				document.querySelector('.mobile-search-button').style.display = 'block'; //Открываем кнопку поиска
+			}
+		}
+	};*/
+
+/*var mySwiper = new Swiper('.swiper-container', {
+  zoom: {
+    maxRatio: 5,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+   }
+});
+
+var swiperSlide = document.getElementsByClassName('swiper-slide')
+for(var index = 0; index< swiperSlide.length; index++){
+swiperSlide[index].addEventListener('mouseover',function(e){
+    mySwiper.zoom.in();
+})
+
+swiperSlide[index].addEventListener('mouseout',function(e){
+    mySwiper.zoom.out();
+})
+}*/
+
+
+
+
 
 
 
