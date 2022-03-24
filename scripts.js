c65d401b02d766c8618cf4ccdfe979c604b8cbe8
TihomirTruvor <46653043+TihomirTruvor@users.noreply.github.com>
2022-03-24 17:28:58 +0300

    Завершение модернизации сайта

--- scripts.js
+++ scripts.js
@@ -47,7 +47,7 @@
 
 	function backToTop() {
 		if (window.pageYOffset > 0) {
-			window.scrollBy(0, -80);
+			window.scrollBy(0, -150);
 			setTimeout(backToTop, 10);
 		}
 	}
@@ -150,6 +150,74 @@ jQuery(window).scroll(function() {
 			jQuery('.language').fadeIn();
 		}
 });
+
+// I know that the code could be better.
+// If you have some tips or improvement, please let me know.
+
+$('.img-parallax').each(function(){
+  var img = $(this);
+  var imgParent = $(this).parent();
+  function parallaxImg () {
+    var speed = img.data('speed');
+    var imgY = imgParent.offset().top;
+    var winY = $(this).scrollTop();
+    var winH = $(this).height();
+    var parentH = imgParent.innerHeight();
+
+
+    // The next pixel to show on screen      
+    var winBottom = winY + winH;
+
+    // If block is shown on screen
+    if (winBottom > imgY && winY < imgY + parentH) {
+      // Number of pixels shown after block appear
+      var imgBottom = ((winBottom - imgY) * speed);
+      // Max number of pixels until block disappear
+      var imgTop = winH + parentH;
+      // Porcentage between start showing until disappearing
+      var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+    }
+    img.css({
+      top: imgPercent + '%',
+      transform: 'translate(-50%, -' + imgPercent + '%)'
+    });
+  }
+  $(document).on({
+    scroll: function () {
+      parallaxImg();
+    }, ready: function () {
+      parallaxImg();
+    }
+  });
+});
+
+id_form_radio_group.onclick = function () {
+	if (document.getElementById('radio-1').checked === true) 
+			{document.getElementById('product-motor').style.display = "flex";
+			document.getElementById('product-display').style.display = "flex";
+			document.getElementById('product-battery').style.display = "flex";
+			document.getElementById('product-controller').style.display = "flex";}
+	if (document.getElementById('radio-2').checked === true) 
+			{document.getElementById('product-motor').style.display = "flex";
+			document.getElementById('product-display').style.display = "none";
+			document.getElementById('product-battery').style.display = "none";
+			document.getElementById('product-controller').style.display = "none";}
+	if (document.getElementById('radio-3').checked === true) 
+			{document.getElementById('product-motor').style.display = "none";
+			document.getElementById('product-display').style.display = "flex";
+			document.getElementById('product-battery').style.display = "none";
+			document.getElementById('product-controller').style.display = "none";}
+	if (document.getElementById('radio-4').checked === true) 
+			{document.getElementById('product-motor').style.display = "none";
+			document.getElementById('product-display').style.display = "none";
+			document.getElementById('product-battery').style.display = "flex";
+			document.getElementById('product-controller').style.display = "none";}
+	if (document.getElementById('radio-5').checked === true) 
+			{document.getElementById('product-motor').style.display = "none";
+			document.getElementById('product-display').style.display = "none";
+			document.getElementById('product-battery').style.display = "none";
+			document.getElementById('product-controller').style.display = "flex";}
+};
 
 
 
